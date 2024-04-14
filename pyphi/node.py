4d4d5e2f676d8cbd11be2200341104a3fc0a7406
Aydan Bailey <ajbailey4@wisc.edu>
2024-04-13 20:29:42 -0500

    Added forward and backward TPMs to node.py
    - Changed constructor to create both forward_tpm and backward_tpm,
      as well as forward and backward versions of intermediate calculations
    - Added forward and backward versions of tpm_off and tpm_on
    - Edited __eq__ to use both forward and backward tpms
    - Edited generate_nodes function to create nodes using the new
      Node constructor, and now requires both forward and backward tpms
      as parameters.

--- pyphi/node.py
+++ pyphi/node.py
@@ -23,14 +23,16 @@ class Node:
     """A node in a subsystem.
 
     Args:
-        tpm (ExplicitTPM): The TPM of the subsystem.
+        forward_tpm (ExplicitTPM): The forward TPM of the subsystem.
+        backward_tpm (ExplicitTPM): The backward TPM of the subsystem.
         cm (np.ndarray): The CM of the subsystem.
         index (int): The node's index in the network.
         state (int): The state of this node.
         node_labels (|NodeLabels|): Labels for these nodes.
 
     Attributes:
-        tpm (ExplicitTPM): The node TPM is an array with shape ``(2,)*(n + 1)``,
+        forward_tpm (ExplicitTPM),
+        backward_tpm (ExplicitTPM): The node TPM is an array with shape ``(2,)*(n + 1)``,
             where ``n`` is the size of the |Network|. The first ``n``
             dimensions correspond to each node in the system. Dimensions
             corresponding to nodes that provide input to this node are of size
@@ -42,7 +44,7 @@ class Node:
             probabilities that the node will be 'ON'.
     """
 
-    def __init__(self, tpm, cm, index, state, node_labels):
+    def __init__(self, forward_tpm, backward_tpm, cm, index, state, node_labels):
         # This node's index in the list of nodes.
         self.index = index
 
@@ -56,14 +58,15 @@ class Node:
         self._inputs = frozenset(get_inputs_from_cm(self.index, cm))
         self._outputs = frozenset(get_outputs_from_cm(self.index, cm))
 
-        # Generate the node's TPM.
+        # Generate the node's TPMs.
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # We begin by getting the part of the subsystem's TPM that gives just
         # the state of this node. This part is still indexed by network state,
         # but its last dimension will be gone, since now there's just a single
         # scalar value (this node's state) rather than a state-vector for all
         # the network nodes.
-        tpm_on = tpm[..., self.index]
+        forward_tpm_on = forward_tpm[..., self.index]
+        backward_tpm_on = backward_tpm[..., self.index]
 
         # TODO extend to nonbinary nodes
         # Marginalize out non-input nodes that are in the subsystem, since the
@@ -71,36 +74,53 @@ class Node:
         # the subsystem's TPM.
 
         # TODO use names rather than indices
-        non_inputs = set(tpm.tpm_indices()) - self._inputs
-        tpm_on = tpm_on.marginalize_out(non_inputs).tpm
+        forward_non_inputs = set(forward_tpm.tpm_indices()) - self._inputs
+        forward_tpm_on = forward_tpm_on.marginalize_out(forward_non_inputs).tpm
+        backward_non_inputs = set(backward_tpm.tpm_indices()) - self._inputs
+        backward_tpm_on = backward_tpm_on.marginalize_out(backward_non_inputs).tpm
 
         # Get the TPM that gives the probability of the node being off, rather
         # than on.
-        tpm_off = 1 - tpm_on
+        forward_tpm_off = 1 - forward_tpm_on
+        backward_tpm_off = 1 - backward_tpm_on
 
         # Combine the on- and off-TPM so that the first dimension is indexed by
         # the state of the node's inputs at t, and the last dimension is
         # indexed by the node's state at t+1. This representation makes it easy
         # to condition on the node state.
-        self.tpm = ExplicitTPM(
-            np.stack([tpm_off, tpm_on], axis=-1),
+        self.forward_tpm = ExplicitTPM(
+            np.stack([forward_tpm_off, forward_tpm_on], axis=-1),
+        )
+        self.backward_tpm = ExplicitTPM(
+            np.stack([backward_tpm_off, backward_tpm_on], axis=-1),
         )
         # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         # Only compute the hash once.
         self._hash = hash(
-            (index, hash(self.tpm), self.state, self._inputs, self._outputs)
+            (index, hash(self.forward_tpm), hash(self.backward_tpm), self.state,
+             self._inputs, self._outputs)
         )
 
     @property
-    def tpm_off(self):
-        """The TPM of this node containing only the 'OFF' probabilities."""
-        return self.tpm[..., 0]
+    def forward_tpm_off(self):
+        """The forward TPM of this node containing only the 'OFF' probabilities."""
+        return self.forward_tpm[..., 0]
 
     @property
-    def tpm_on(self):
-        """The TPM of this node containing only the 'ON' probabilities."""
-        return self.tpm[..., 1]
+    def backward_tpm_off(self):
+        """The backward TPM of this node containing only the 'OFF' probabilities."""
+        return self.backward_tpm[..., 0]
+
+    @property
+    def forward_tpm_on(self):
+        """The forward TPM of this node containing only the 'ON' probabilities."""
+        return self.forward_tpm[..., 1]
+
+    @property
+    def backward_tpm_on(self):
+        """The backward TPM of this node containing only the 'ON' probabilities."""
+        return self.backward_tpm[..., 1]
 
     @property
     def inputs(self):
@@ -135,7 +155,8 @@ class Node:
         """
         return (
             self.index == other.index
-            and self.tpm.array_equal(other.tpm)
+            and self.forward_tpm.array_equal(other.forward_tpm)
+            and self.backward_tpm.array_equal(other.backward_tpm)
             and self.state == other.state
             and self.inputs == other.inputs
             and self.outputs == other.outputs
@@ -156,11 +177,12 @@ class Node:
         return self.index
 
 
-def generate_nodes(tpm, cm, network_state, indices, node_labels=None):
+def generate_nodes(forward_tpm, backward_tpm, cm, network_state, indices, node_labels=None):
     """Generate |Node| objects for a subsystem.
 
     Args:
-        tpm (ExplicitTPM): The system's TPM
+        forward_tpm (ExplicitTPM): The system's forward_TPM
+        backward_tpm (ExplicitTPM): The system's backward_TPM
         cm (np.ndarray): The corresponding CM.
         network_state (tuple): The state of the network.
         indices (tuple[int]): Indices to generate nodes for.
@@ -177,7 +199,7 @@ def generate_nodes(tpm, cm, network_state, indices, node_labels=None):
     node_state = utils.state_of(indices, network_state)
 
     return tuple(
-        Node(tpm, cm, index, state, node_labels)
+        Node(forward_tpm, backward_tpm, cm, index, state, node_labels)
         for index, state in zip(indices, node_state)
     )
 
