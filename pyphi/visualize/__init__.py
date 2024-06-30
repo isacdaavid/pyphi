735033a977abb802bf802eee285b2cea1a394444
Will Mayner <wmayner@gmail.com>
2024-06-30 09:39:29 +0900

    Move Ising viz functions to `visualize.ising`

--- pyphi/visualize/__init__.py
+++ pyphi/visualize/__init__.py
@@ -17,3 +17,4 @@ from .dynamics import plot_dynamics
 from .connectivity import plot_graph, plot_subsystem
 from .phi_structure import plot_phi_structure, highlight_phi_fold
 from . import phi_structure
+from . import ising
